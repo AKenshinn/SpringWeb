cd14ddd341b3923932e33b4119cd3f3971072e83
Anat <anat.pantera@gmail.com>
2016-01-20 22:46:36 +0700

    add RestController of Teacher

---
+++ src/main/java/com/web/resources/TeacherResource.java
@@ -0,0 +1,31 @@
+package com.web.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.db.entities.Teacher;
+import com.db.services.TeacherService;
+
+@RestController
+public class TeacherResource {
+
+	private Logger LOGGER = LoggerFactory.getLogger(TeacherResource.class);
+	
+	@Autowired
+	private TeacherService teacherService;
+	
+	@RequestMapping(value="/api/teachers", method={RequestMethod.GET})
+	public List<Teacher> findAllTeachers() {
+		LOGGER.info("RestController : findAllTeachers()");
+		return teacherService.findAll();
+		
+	}
+	
+	
+}
