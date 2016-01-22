9bc1148613af25791ff0551547c99be385025c69
Anat <anat.pantera@gmail.com>
2016-01-22 10:39:50 +0700

    add Student CRUD

--- src/main/java/com/web/controllers/StudentController.java
+++ src/main/java/com/web/controllers/StudentController.java
@@ -1,15 +1,119 @@
 package com.web.controllers;
 
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.db.entities.Student;
+import com.db.services.StudentService;
 
 @Controller
 public class StudentController {
+
+	private Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
 	
-	@RequestMapping(value="/student", method={RequestMethod.GET})
-	public String main() {
-		return "student";
+	@Autowired
+	private StudentService studentService;
+	
+	@ModelAttribute(value="students")
+	private List<Student> findAllStudents() {
+		return studentService.findAll();
 	}
 	
+	@RequestMapping(value = "/student", method = { RequestMethod.GET })
+	public ModelAndView main() {
+		LOGGER.info("Controller : main()");
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("student");
+		modelAndView.addObject("student", new Student());
+		
+		return modelAndView;
+		
+	}
+
+	@RequestMapping(value = "/student/create", method={RequestMethod.POST})
+	public String create(@ModelAttribute(value="student") Student student) {
+		LOGGER.info("Controller : create()");
+		LOGGER.info("ID:" + student.getId() + " FIRST_NAME:" + student.getFirstName() + 
+				" LAST_NAME:" + student.getLastName());
+		
+		if (!StringUtils.isEmpty(student.getFirstName()) && !student.getFirstName().equals("") 
+				&& !StringUtils.isEmpty(student.getLastName()) && !student.getLastName().equals("")) {
+			student.setCreatedDateTime(new Date());
+			student.setUpdatedDateTime(new Date());
+			
+			studentService.insert(student);
+			
+		}
+		
+		return "redirect:/student";
+		
+	}
+	
+	@RequestMapping(value="/student/update/{id}", method={RequestMethod.GET})
+	public ModelAndView initUpdate(@PathVariable(value="id") Long id) {
+		LOGGER.info("Find ID:" + id);
+		
+		Student student = studentService.findById(id);
+		if (student == null) {
+			LOGGER.info("Can't find when ID:" + id);
+			student = new Student();
+		}
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("student");
+		modelAndView.addObject("student", student);
+		
+		return modelAndView;
+		
+	}
+	
+	@RequestMapping(value="/student/update", method={RequestMethod.POST})
+	public String update(@ModelAttribute(value="student") Student student) {
+		LOGGER.info("Controller : update()");
+		LOGGER.info("ID:" + student.getId() + " FIRST_NAME:" + student.getFirstName() + 
+				" LAST_NAME:" + student.getLastName());
+		
+		Student update = studentService.findById(student.getId());
+		
+		if (!StringUtils.isEmpty(student.getFirstName()) && !student.getFirstName().equals("") 
+				&& !StringUtils.isEmpty(student.getLastName()) && !student.getLastName().equals("")) {
+			update.setFirstName(student.getFirstName());
+			update.setLastName(student.getLastName());
+			update.setUpdatedDateTime(new Date());
+			
+			studentService.update(update);
+			
+		}
+		
+		return "redirect:/student";
+		
+	}
+	
+	@RequestMapping(value="/student/delete/{id}", method={RequestMethod.GET})
+	public String deleteById(@PathVariable(value="id") Long id) {
+		LOGGER.info("Find ID:" + id);
+		
+		Student student = studentService.findById(id);
+		if (student == null) {
+			LOGGER.info("Can't find when ID:" + id);
+		} else {
+			studentService.deleteById(id);
+		}
+		
+		return "redirect:/student";
+		
+	}
+
 }
--- src/main/webapp/WEB-INF/views/student.jsp
+++ src/main/webapp/WEB-INF/views/student.jsp
@@ -1,15 +1,92 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="f"%>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Student</title>
 </head>
 <body>
+
+	<c:choose>
+	    <c:when test="${empty student.id}">
+	        <c:set var="action" scope="session" value="create"/>
+	        <c:set var="title" scope="session" value="Create"/>
+	    </c:when>
+	    <c:otherwise>
+	        <c:set var="action" scope="session" value="update"/>
+	        <c:set var="title" scope="session" value="Update"/>
+	    </c:otherwise>
+	</c:choose>
+	
 	<div class="row">
 		<div class="col-sm-12">
-			<h1>Student</h1>	
+			<div class="page-header">
+				<h1>Student</h1>
+			</div>	
 		</div>
 	</div>
+	
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h4>${title} Student</h4>
+				</div>
+			  	<div class="panel-body">
+			   		<f:form action="${pageContext.request.contextPath}/student/${action}" method="POST" commandName="student" 
+			   			cssClass="form-inline">
+			   			<f:hidden path="id" />
+						<div class="form-group">
+							<label for="firstName">First Name : </label>
+							<f:input type="text" path="firstName" cssClass="form-control" id="firstName" placeholder="First Name" value="${student.firstName}"/>
+						</div>
+						&nbsp;
+						<div class="form-group">
+							<label for="lastName">Last Name : </label>
+							<f:input type="text" path="lastName" cssClass="form-control" id="lastName" placeholder="Last Name" value="${student.lastName}"/>
+						</div>
+						<button type="submit" class="btn btn-primary">${title}</button>
+					</f:form>
+			  	</div>
+			</div>
+			
+			<hr/>
+		</div>
+	</div>
+	
+	<br/>
+	
+	<div class="row">
+		<div class=col-sm-12>
+			<table class="table table-bordered table-responsive">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Created</th>
+						<th>Updated</th>
+						<th colspan="2">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<c:forEach items="${students}" var="student">
+						<tr>
+							<td>${student.id}</td>
+							<td>${student.firstName}</td>
+							<td>${student.lastName}</td>
+							<td>${student.createdDateTime}</td>
+							<td>${student.updatedDateTime}</td>
+							<td><a class="btn btn-warning" href="${pageContext.request.contextPath}/student/update/${student.id}">Update</a></td>
+							<td><a class="btn btn-danger" href="${pageContext.request.contextPath}/student/delete/${student.id}">Delete</a></td>
+						</tr>
+                  	</c:forEach>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	
 </body>
 </html>
